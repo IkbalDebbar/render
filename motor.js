60bbcedf1a2b5b40ed22887c4fdc8378be2b3407
IkbalDebbar <debbarikbal8@gmail.com>
2026-04-15 14:48:39 +0200

    motor.js

--- motor.js
+++ motor.js
@@ -1,33 +1,19 @@
-const Gpio = require("pigpio").Gpio;
+// motor.js (SAFE FOR WINDOWS + RENDER TESTING)
 
-const IN1 = new Gpio(17, { mode: Gpio.OUTPUT });
-const IN2 = new Gpio(18, { mode: Gpio.OUTPUT });
-const IN3 = new Gpio(22, { mode: Gpio.OUTPUT });
-const IN4 = new Gpio(23, { mode: Gpio.OUTPUT });
-
-function forward() {
-  IN1.digitalWrite(1); IN2.digitalWrite(0);
-  IN3.digitalWrite(1); IN4.digitalWrite(0);
+function initMotor() {
+  console.log("Motor module running (simulation mode)");
 }
 
-function backward() {
-  IN1.digitalWrite(0); IN2.digitalWrite(1);
-  IN3.digitalWrite(0); IN4.digitalWrite(1);
-}
-
-function left() {
-  IN1.digitalWrite(0); IN2.digitalWrite(1);
-  IN3.digitalWrite(1); IN4.digitalWrite(0);
-}
-
-function right() {
-  IN1.digitalWrite(1); IN2.digitalWrite(0);
-  IN3.digitalWrite(0); IN4.digitalWrite(1);
+function move(action, speed) {
+  console.log("Motor:", action, speed);
 }
 
 function stop() {
-  IN1.digitalWrite(0); IN2.digitalWrite(0);
-  IN3.digitalWrite(0); IN4.digitalWrite(0);
+  console.log("Motor stopped");
 }
 
-module.exports = { forward, backward, left, right, stop };
+module.exports = {
+  initMotor,
+  move,
+  stop
+};
